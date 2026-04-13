0a83d249329ec70d7f20b4e61ffdba4767c057b3
Pritesh Nakra <Priteshnakra@gmail.com>
2026-04-12 23:42:12 -0400

    Fix Vite allowed hosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: 3000,
+    allowedHosts: ['enchanting-encouragement-production-64c7.up.railway.app'],
+  },
   server: {
     port: 5173,
     proxy: {
